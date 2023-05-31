4b3aa4be40a38e8d6eed2ce52c6e24560a492926
JAVA-DEVELOPMENT-SSK <saisrikongara1198@gmail.com>
2023-05-31 17:06:03 -0500

    Student

---
+++ Student/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String args[]){
+        Student stud=new Student();
+        stud.setName("Mahinder Singh Dhoni");
+        System.out.println("Student name is " +stud.getName());
+
+    }
+}
---
+++ Student/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    String Name;
+    public String getName(){
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+}
