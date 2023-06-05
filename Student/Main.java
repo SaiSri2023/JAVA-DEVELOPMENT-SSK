3a403260526d63a322ba25e5380c30f1271cb931
JAVA-DEVELOPMENT-SSK <saisrikongara1198@gmail.com>
2023-06-05 09:14:15 -0500

    week1 and updated structure

--- Student/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String args[]){
-        Student stud=new Student();
-        stud.setName("Mahinder Singh Dhoni");
-        System.out.println("Student name is " +stud.getName());
-
-    }
-}
