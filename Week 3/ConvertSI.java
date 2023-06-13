006b7ddf1d2a3e7c1a6169624405095375a65f02
JAVA-DEVELOPMENT-SSK <saisrikongara1198@gmail.com>
2023-06-13 14:34:34 -0500

    Week 3

---
+++ Week 3/ConvertSI.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ConvertSI {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String ");
+        String str = sc.nextLine();
+
+        try {
+            int input = Integer.parseInt(str);
+            System.out.println("The  string "+ str+"and result are " + input);
+        }
+        catch(Exception e){
+            System.out.println("There is an exception");
+        }
+    }
+}
